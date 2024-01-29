427988b83ccb2b31eecb51084a8d07f1e6e4ea99
kiran <kiran@DESKTOP-EPAVBKE>
2024-01-29 21:43:39 +0530

    commit1

---
+++ src/test/java/com/actiitme/testscript/CustomerModule.java
@@ -0,0 +1,12 @@
+package com.actiitme.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	@Test
+	public void createCustomer() {
+		Reporter.log("Create customer");
+	}
+
+}
